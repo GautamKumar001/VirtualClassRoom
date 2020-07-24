fdccce71a79eb6dcd512f9e767d46f18724325cf
Gautam Kumar <mailmekumargautam665@gmail.com>
2020-07-24 22:17:23 +0530

    third commit

--- resources/views/VideoScreen/Room.blade.php
+++ resources/views/VideoScreen/Room.blade.php
@@ -21,14 +21,12 @@
             console.log(`Connected to Room: ${room.name}`);
             const localParticipant = room.localParticipant;
             console.log(`Connected to the Room as LocalParticipant "${localParticipant.identity}"`);
-            let count=0;
+            let count = 0;
             room.participants.forEach(participant => {
                 count++;
                 console.log(`Participant "${participant.identity}" is connected to the Room`);
                 participantConnected(participant, room,count);
             });
-            room.participants.forEach(participantConnected);
-
             var previewContainer = document.getElementById(room.localParticipant.sid);
             if (!previewContainer || !previewContainer.querySelector('video')) {
                 participantConnected(room.localParticipant, room);
@@ -36,9 +34,9 @@
 
             room.on('participantConnected', function (participant) {
                 count++;
-                console.log("Participant count " +count );
+                console.log("Participant count " + count);
                 console.log("Joining:");
-                participantConnected(participant, room,count);
+                participantConnected(participant, room, count);
             });
 
             room.on('participantDisconnected', function (participant) {
@@ -47,13 +45,13 @@
             });
         });
         // additional functions will be added after this point
-        function participantConnected(participant, room,count) {
-            console.log("Participant count " +count );
+        function participantConnected(participant, room, count) {
+            console.log("Participant count " + JSON.stringify(count));
             console.log('Participant connected' + participant);
             console.log('Room deatils' + room);
             const localParticipant = room.localParticipant;
             if (participant == localParticipant) {
-                console.log("localParticipant count " +count );
+                console.log("localParticipant count " + count);
                 console.log('Participant connected is local' + participant);
                 const div = document.createElement('div');
                 div.id = participant.sid;
@@ -74,53 +72,42 @@
                 document.getElementById('media-container').appendChild(div);
 
             } else {
-                console.log("Remote Participant count" + count );
-
-                let counts=count-1;
-                console.log("Remote Participant count:=" + counts );
+                console.log("Remote Participant count:=" + count);
+if(count==={})
+{
+    count=1;
+    console.log("Remote Participant countnan:=" + count);
+}
+                let counts = count - 1;
+                console.log("Remote Participant counts:=" + counts);
 
                 console.log(`Participant "${participant.identity}" is connected to the Room`);
                 const div = document.getElementById('video-div');
                 // When a Participant joins the Room, log the event.
-               // When a Participant adds a Track, attach it to the DOM.
-   participant.on('trackAdded', function (track) {
-if(counts==undefined)
-{
-return;
-}
-            else{
-                console.log("Remote Participant count2=" + counts );
+                // When a Participant adds a Track, attach it to the DOM.
+                participant.on('trackAdded', function (track) {
+                    console.log("Remote Participant count2=" + counts);
                     div.appendChild(track.attach());
                     var video = div.getElementsByTagName("video")[counts];
-                        if (video) {
-                            console.log("video is displaying");
-                            video.id=track.id;
-                            video.setAttribute("style","height: 100px;width: 220px;");
-                        }
-                    document.getElementById('media-container').appendChild(div);
-            }
+                    if (video) {
+                        video.id = track.id;
+                        video.setAttribute("style", "height: 100px;width: 220px;");
+                    }
+                    document.getElementById('media-container').appendChild(div)
+
                 });
                 // Attach the Tracks of the Room's Participants.
                 participant.tracks.forEach(function (track) {
-                    console.log("Remote Participant count1=" + counts );
-                    console.log('trackid:=>'+track.id);
-if(counts==undefined)
-{
-    return;
-}
-else{
-    div.appendChild(track.attach());
-                      var video = div.getElementsByTagName("video")[counts];
+                    console.log("Remote Participant count1=" + counts);
+                    console.log('trackid:=>' + track.id);
+                        div.appendChild(track.attach());
+                        var video = div.getElementsByTagName("video")[counts];
                         if (video) {
-                            video.id=track.id;
+                            video.id = track.id;
                             video.setAttribute("style", "height: 100px;width: 220px;");
                         }
-                    document.getElementById('media-container').appendChild(div);
-}
-
-
+                        document.getElementById('media-container').appendChild(div)
                 });
-
                 participant.on('trackRemoved', trackRemoved);
             }
         }
